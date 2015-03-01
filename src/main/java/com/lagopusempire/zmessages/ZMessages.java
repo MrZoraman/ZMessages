ae4820f8b4f368554430f90bbf0e9a1561e5d143
Zora <MrZoraman@Gmail.com>
2015-02-28 18:57:07 -0700

    Plugin.yml added. Main class created.

---
+++ src/main/java/com/lagopusempire/zmessages/ZMessages.java
@@ -0,0 +1,16 @@
+package com.lagopusempire.zmessages;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author MrZoraman
+ */
+public class ZMessages extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        
+    }
+}
