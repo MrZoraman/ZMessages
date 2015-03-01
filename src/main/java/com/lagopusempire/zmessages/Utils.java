e1ed7704413c9e8db9dc6647f5293e694b233608
Zora <MrZoraman@Gmail.com>
2015-02-28 21:54:14 -0700

    Made the matchCommandSender method

--- src/main/java/com/lagopusempire/zmessages/Utils.java
+++ src/main/java/com/lagopusempire/zmessages/Utils.java
@@ -1,6 +1,9 @@
 package com.lagopusempire.zmessages;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.UUID;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -49,5 +52,28 @@ public class Utils
             return null;
         else
             return ((Player) sender).getUniqueId();
+    }
+    
+    public static CommandSender matchCommandSender(String name)
+    {
+        name = name.toLowerCase();
+        if(name.equals("console") || name.equals("terminal") || name.equals("server"))
+        {
+            return Bukkit.getConsoleSender();
+        }
+        
+        Collection<? extends Player> onlinePlayers = Bukkit.getOnlinePlayers();
+        Iterator<? extends Player> it = onlinePlayers.iterator();
+        while(it.hasNext())
+        {
+            Player p = it.next();
+            String playerName = p.getName().toLowerCase();
+            if(playerName.equals(name))
+            {
+                return p;
+            }
+        }
+        
+        return null;
     }
 }
