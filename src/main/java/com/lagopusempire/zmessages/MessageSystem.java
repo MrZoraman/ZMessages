1bba001049ff7431f8f558e141ae2bd3fd8462fb
Zora <MrZoraman@Gmail.com>
2015-02-28 20:36:03 -0700

    Added constructor

--- src/main/java/com/lagopusempire/zmessages/MessageSystem.java
+++ src/main/java/com/lagopusempire/zmessages/MessageSystem.java
@@ -1,5 +1,6 @@
 package com.lagopusempire.zmessages;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 /**
@@ -8,6 +9,15 @@ import org.bukkit.entity.Player;
  */
 public class MessageSystem
 {
+    private final String format_sender, format_reciever, format_socialspy;
+    
+    public MessageSystem(FileConfiguration config)
+    {
+        format_sender = config.getString("format.sender");
+        format_reciever = config.getString("format.reciever");
+        format_socialspy = config.getString("format.socialspy");
+    }
+    
     public void sendMessage(Player from, Player to, String message)
     {
         
