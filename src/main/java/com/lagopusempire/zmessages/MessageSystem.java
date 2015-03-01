34df4201a69227c680624fc430a9e7f020fcca9d
Zora <MrZoraman@Gmail.com>
2015-02-28 20:22:54 -0700

    Added Message System

---
+++ src/main/java/com/lagopusempire/zmessages/MessageSystem.java
@@ -0,0 +1,15 @@
+package com.lagopusempire.zmessages;
+
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author MrZoraman
+ */
+public class MessageSystem
+{
+    public void sendMessage(Player from, Player to, String message)
+    {
+        
+    }
+}
